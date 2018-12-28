5e35ccea5eca6158adf00628a761e0c7cbf6edcf
realsazzad <realsazzad@gmail.com>
2018-12-28 22:29:48 +0600

    first commit-sazzad

--- QDsimple/src/qdsimple/QDsimple.java
+++ QDsimple/src/qdsimple/QDsimple.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package qdsimple;
 
 import javafx.application.Application;
@@ -25,6 +21,7 @@ public class QDsimple extends Application {
         
         stage.setScene(scene);
         stage.show();
+        //sazzad
     }
 
     /**
