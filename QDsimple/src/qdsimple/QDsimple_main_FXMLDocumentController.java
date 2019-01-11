3a0511b1cc60a3b79356242dba67ebe32bf420e5
Mashruk Kabir <mashruk28@gmail.com>
2019-01-11 20:05:41 +0600

    Renamed variable names from Capital to small

--- QDsimple/src/qdsimple/QDsimple_main_FXMLDocumentController.java
+++ QDsimple/src/qdsimple/QDsimple_main_FXMLDocumentController.java
@@ -29,9 +29,7 @@ import com.jfoenix.controls.JFXDialogLayout;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.events.JFXDialogEvent;
 import javafx.event.EventHandler;
-import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.effect.BoxBlur;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
@@ -40,22 +38,10 @@ public class QDsimple_main_FXMLDocumentController implements Initializable {
     
     // FXML document loaded variables
     @FXML
-    private BorderPane BorderPane_Main;
+    private BorderPane borderpane_main;
     @FXML
-    private MenuBar MenuBar_BorderPane_Main;
-    @FXML
-    private Menu File_MenuBar_BorderPane_Main;
-    @FXML
-    private MenuItem Close_File_MenuBar_BorderPane_Main;
-    @FXML
-    private Menu Help_MenuBar_BorderPane_Main;
-    @FXML
-    private MenuItem About_Help_MenuBar_BorderPane_Main;
-    @FXML
-    private AnchorPane AnchorPaneRight_BorderPane_Main;
-    @FXML
-    private StackPane StackPane_Dialog;
-    double StackPane_Dialog_h, StackPane_Dialog_w;
+    private StackPane stackpane_dialog;
+    double stackpane_dialog_h, stackpane_dialog_w;
     @FXML
     private Button cube_button;
     @FXML
@@ -71,39 +57,47 @@ public class QDsimple_main_FXMLDocumentController implements Initializable {
     @FXML
     private ImageView line_button;
     @FXML
-    private ScrollPane scroll_pane;
+    private MenuBar menubar_borderpane_main;
     @FXML
-    private Canvas canvas_panel;
+    private Menu file_menubar_borderpane_main;
+    @FXML
+    private MenuItem close_file_menubar_borderpane_main;
+    @FXML
+    private Menu help_menubar_borderpane_main;
+    @FXML
+    private MenuItem about_help_menubar_borderpane_main;
+    @FXML
+    private AnchorPane anchorpaneright_borderpane_main;
             
             
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Dynamically change window size whenever window size is changed
-        StackPane_Dialog_w = StackPane_Dialog.widthProperty().doubleValue();
-        StackPane_Dialog_h = StackPane_Dialog.heightProperty().doubleValue();
-        Window_resize_listener_f();
+        stackpane_dialog_w = stackpane_dialog.widthProperty().doubleValue();
+        stackpane_dialog_h = stackpane_dialog.heightProperty().doubleValue();
+        window_resize_listener_f();
         
         
     }    
     
-    private void Window_resize_listener_f() {
-        StackPane_Dialog.widthProperty().addListener((obs, oldVal, newVal) ->{
-            StackPane_Dialog_w = newVal.doubleValue();
-            BorderPane_Main.setPrefWidth(StackPane_Dialog_w);
+    private void window_resize_listener_f() {
+        stackpane_dialog.widthProperty().addListener((obs, oldVal, newVal) ->{
+            stackpane_dialog_w = newVal.doubleValue();
+            borderpane_main.setPrefWidth(stackpane_dialog_w);
         });
-        StackPane_Dialog.heightProperty().addListener((obs, oldVal, newVal) ->{
-            StackPane_Dialog_h = newVal.doubleValue();
-            BorderPane_Main.setPrefHeight(StackPane_Dialog_h);
+        stackpane_dialog.heightProperty().addListener((obs, oldVal, newVal) ->{
+            stackpane_dialog_h = newVal.doubleValue();
+            borderpane_main.setPrefHeight(stackpane_dialog_h);
         });
     }
 
     @FXML
-    private void Activated_Close_File_MenuBar_BorderPane_Main_f(ActionEvent event) {
+    private void activated_close_file_menubar_borderpane_main_f(ActionEvent event) {
         QDsimple.get_current_instance_QDsimple().exit_program();
     }
 
     @FXML
-    private void Activated_About_Help_MenuBar_BorderPane_Main_f(ActionEvent event) {
+    private void activated_about_help_menubar_borderpane_main_f(ActionEvent event) {
         // To blur the background when dialogue box pops in
         BoxBlur blur = new BoxBlur(3, 3, 3);
         
@@ -114,7 +108,7 @@ public class QDsimple_main_FXMLDocumentController implements Initializable {
                 + "development by the group Artificial Indie Collaborations. For reference to the source code, contact AI collab.\n"));
         
         
-        JFXDialog About_dialog = new JFXDialog(StackPane_Dialog, About_content, JFXDialog.DialogTransition.CENTER);
+        JFXDialog About_dialog = new JFXDialog(stackpane_dialog, About_content, JFXDialog.DialogTransition.CENTER);
         JFXButton About_button = new JFXButton("Okay");
         About_button.setStyle(" -fx-background-color: slateblue; -fx-text-fill: white; ");
         
@@ -131,11 +125,11 @@ public class QDsimple_main_FXMLDocumentController implements Initializable {
         
         // blurred background when dialog box opened
         About_dialog.setOnDialogOpened((JFXDialogEvent event_opened) ->{
-            BorderPane_Main.setEffect(blur);
+            borderpane_main.setEffect(blur);
         });
         // de - blurred background when dialog box opened
         About_dialog.setOnDialogClosed((JFXDialogEvent event_closed) ->{
-            BorderPane_Main.setEffect(null);
+            borderpane_main.setEffect(null);
         });
     }
     
